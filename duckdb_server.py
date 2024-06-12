e782b42c3c051adde90c9ca3807af1205b71002e
PJ Kellym <kellypj24@gmail.com>
2024-06-11 21:42:59 -0400

    failed approach with python

--- duckdb_server.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-
-# Start DuckDB server
-con = duckdb.connect(database=':memory:', port=5432)
-
-# Keep the server running
-con.execute('SELECT 1')
-input("Press Enter to stop the server...")
